a66509e95585dbf41859be3a25103e4ac05f0cdf
GrimInside <grimbehindspects@gmail.com>
2021-09-12 04:12:01 +0530

    added analog

--- script.js
+++ script.js
@@ -4,18 +4,24 @@ const hourHand = document.querySelector('[data-hour-hand]')
 const minuteHand = document.querySelector('[data-minute-hand]')
 const secondHand = document.querySelector('[data-second-hand]')
 
+
 function setClock() {
   const currentDate = new Date()
+  let time = currentDate.getHours() + ':' + currentDate.getMinutes() +':' + currentDate.getSeconds();
+  let date = currentDate.toLocaleDateString();
   const secondsRatio = currentDate.getSeconds() / 60
   const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
   const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
   setRotation(secondHand, secondsRatio)
   setRotation(minuteHand, minutesRatio)
   setRotation(hourHand, hoursRatio)
+  document.getElementById('time').innerHTML = time;
+  document.getElementById('date').innerHTML = date;
 }
 
 function setRotation(element, rotationRatio) {
   element.style.setProperty('--rotation', rotationRatio * 360)
 }
+
 
 setClock()
